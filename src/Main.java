3cc2d9c5c066e98408dfdcb213b4ab4ff7574d41
leonardoalbuquerque <leonardo.lopes.albuquerque@gmail.com>
2016-09-13 09:30:21 -0300

    now the output image is a ppm image

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int numberOfLinesToGoBack = 3 * 2;
+        int scaleFactor = 2;
+        int numberOfLinesToGoBack = 3 * scaleFactor;
 
         FileInputStream in;
 
@@ -33,13 +35,27 @@ public class Main {
         int stripSize = decoder.getHeight() / 10;
 
         BufferedImage originalStripe = new BufferedImage(decoder.getWidth(), stripSize, BufferedImage.TYPE_INT_RGB);
-        BufferedImage outputImage = new BufferedImage(decoder.getWidth() * 2, decoder.getHeight() * 2, BufferedImage.TYPE_INT_RGB);
+        //BufferedImage outputImage = new BufferedImage(decoder.getWidth() * scaleFactor, decoder.getHeight() * scaleFactor, BufferedImage.TYPE_INT_RGB);
 
         int[] raster = new int[decoder.getWidth() * stripSize];
 
-        int scalatedStripSize = stripSize * 2;
+        int scalatedStripSize = stripSize * scaleFactor;
         int scalatedStripStart = 0;
         int scalatedStripEnd = scalatedStripSize;
+
+        FileOutputStream outputFile;
+        try {
+            outputFile = new FileOutputStream("output.ppm");
+        } catch (Exception e) {
+            System.out.println("erro ao abrir o arquivo de saida: " + e.getMessage());
+            return;
+        }
+
+        PpmEncoder encoder = new PpmEncoder(outputFile, decoder.getWidth() * scaleFactor, decoder.getHeight() * scaleFactor);
+        encoder.writeHeader();
+
+        //imagem com apenas os pixels bons
+        BufferedImage goodPixels = new BufferedImage(decoder.getWidth() * scaleFactor, (stripSize * scaleFactor) - numberOfLinesToGoBack, BufferedImage.TYPE_INT_RGB);
 
         for (int i = 0; i <= 10; i++) {
 
@@ -51,23 +67,31 @@ public class Main {
             }
 
             setPixels(originalStripe, decoder.getWidth(), stripSize, raster);
-            BufferedImage resizedStripe = resize(originalStripe, decoder.getWidth() * 2, stripSize * 2);
-
+            BufferedImage resizedStripe = resize(originalStripe, decoder.getWidth() * scaleFactor, stripSize * scaleFactor);
 
             if (i == 0) {
-                transferPixels(resizedStripe, outputImage, scalatedStripStart, scalatedStripEnd, 0);
-            }else if(i < 10){
-                transferPixels(resizedStripe, outputImage, scalatedStripStart, scalatedStripEnd - numberOfLinesToGoBack, numberOfLinesToGoBack);
-            }else{
-                transferPixels(resizedStripe, outputImage, scalatedStripStart, scalatedStripEnd, numberOfLinesToGoBack);
+                //transferPixels(resizedStripe, outputImage, scalatedStripStart, scalatedStripEnd, 0);
+
+
+                transferGoodPixels(resizedStripe, goodPixels, 0);
+            } else if (i < 10) {
+                //transferPixels(resizedStripe, outputImage, scalatedStripStart, scalatedStripEnd - numberOfLinesToGoBack, numberOfLinesToGoBack);
+
+                goodPixels = new BufferedImage(decoder.getWidth() * scaleFactor, scalatedStripEnd - numberOfLinesToGoBack - scalatedStripStart, BufferedImage.TYPE_INT_RGB);
+                transferGoodPixels(resizedStripe, goodPixels, numberOfLinesToGoBack);
+            } else {
+                //transferPixels(resizedStripe, outputImage, scalatedStripStart, scalatedStripEnd, numberOfLinesToGoBack);
+
+                goodPixels = new BufferedImage(decoder.getWidth() * scaleFactor, scalatedStripEnd - scalatedStripStart, BufferedImage.TYPE_INT_RGB);
+                transferGoodPixels(resizedStripe, goodPixels, numberOfLinesToGoBack);
             }
 
             //volta as linhas da imagem original, para poder ter pixels a mais para processar
             try {
-                if(i == 0){
+                if (i == 0) {
                     decoder.goBackNLines(numberOfLinesToGoBack);
-                }else{
-                    decoder.goBackNLines(numberOfLinesToGoBack/2);
+                } else {
+                    decoder.goBackNLines(numberOfLinesToGoBack / scaleFactor);
                 }
             } catch (Exception e) {
                 System.out.println("Erro ao voltar linhas " + e.getMessage());
@@ -79,25 +103,27 @@ public class Main {
                 scalatedStripEnd += scalatedStripSize - numberOfLinesToGoBack;
             } else {
                 //+ the 6 lines taken unecessarily on the last loop
-                stripSize = decoder.getHeight() - scalatedStripEnd/2 + numberOfLinesToGoBack;
+                stripSize = decoder.getHeight() - scalatedStripEnd / scaleFactor + numberOfLinesToGoBack;
                 scalatedStripStart = scalatedStripEnd - numberOfLinesToGoBack;
-                scalatedStripEnd = decoder.getHeight() * 2;
+                scalatedStripEnd = decoder.getHeight() * scaleFactor;
 
-                raster = new int[decoder.getWidth() * stripSize*2];
+                raster = new int[decoder.getWidth() * stripSize * scaleFactor];
                 originalStripe = new BufferedImage(decoder.getWidth(), stripSize, BufferedImage.TYPE_INT_RGB);
             }
 
             long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 
             System.out.println("memória usada: " + usedMemory);
+
+            encoder.writeBufferedImage(goodPixels);
         }
 
-        try {
+        /*try {
             File outputfile = new File("saved.png");
             ImageIO.write(outputImage, "png", outputfile);
         } catch (Exception e) {
             System.out.println("Erro ao gerar png: " + e.getMessage());
-        }
+        }*/
     }
 
     private static void setPixels(BufferedImage bufferedImage, int imageWidth, int stripSize, int[] raster) {
@@ -135,5 +161,19 @@ public class Main {
             }
             inputY++;
         }
+    }
+
+    private static void transferGoodPixels(BufferedImage input, BufferedImage output, int offset) {
+
+        if (offset > 0) {
+            offset--; //fix for 0 started arrays
+        }
+
+        for (int y = 0; y < output.getHeight(); y++) {
+            for (int x = 0; x < output.getWidth(); x++) {
+                output.setRGB(x, y, input.getRGB(x, y + offset));
+            }
+        }
+
     }
 }
